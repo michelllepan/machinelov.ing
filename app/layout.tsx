b90874d8fee836cb9c2173e74fe4b6acf541372e
Michelle Pan <michellepan8@gmail.com>
2026-02-13 13:57:06 -0800

    tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,24 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://machinelov.ing"),
   title: "ml valentines",
   description: "machine learning is a love language too",
+  openGraph: {
+    title: "ml valentines",
+    description: "machine learning is a love language too",
+    images: [
+      {
+        url: "/og/base.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "ml valentines",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "ml valentines",
+    description: "machine learning is a love language too",
+    images: ["/og/base.jpeg"],
+  },
 };
 
 export default function RootLayout({
